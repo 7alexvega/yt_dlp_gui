03e705f5e982286c092c4b611e7829d4e3ad5c41
7av <7alexvega@gmail.com>
2024-10-06 12:26:05 -0700

    created the mainwindow ui

---
+++ app/main_window.py
@@ -0,0 +1,114 @@
+from PyQt6.QtCore import Qt
+from PyQt6 import QtCore, QtWidgets
+from PyQt6.QtWidgets import QApplication, QStyle, QSizePolicy, QTableView
+
+
+class UIMainWindow(object):
+    def setup_ui(self, MainWindow):
+        # App Name and Size
+        MainWindow.setWindowTitle('yt_dlp gui')
+        MainWindow.resize(1000, 600)
+
+        # Main layout for the central widget
+        self.central_widget = QtWidgets.QWidget(parent=MainWindow)
+        self.main_layout = QtWidgets.QVBoxLayout(self.central_widget)
+
+        # Config Section
+        self.layout_config_section = QtWidgets.QHBoxLayout()
+
+        # Config Section - Download Format Layout
+        self.layout_download_format = QtWidgets.QVBoxLayout()
+        self.label_download_format = QtWidgets.QLabel('Default Format:')
+        self.combo_box_download_formats = QtWidgets.QComboBox()
+        self.layout_download_format.addWidget(self.label_download_format)
+        self.layout_download_format.addWidget(self.combo_box_download_formats)
+
+        # Config Section - Save Directory Layout
+        self.layout_save_directory = QtWidgets.QVBoxLayout()
+        self.layout_save_directory_header = QtWidgets.QHBoxLayout()
+        self.button_directory_selector = QtWidgets.QPushButton()
+        self.button_directory_selector.setMaximumSize(QtCore.QSize(20, 20))
+        self.button_directory_selector_icon = QApplication.style().standardIcon(
+            QStyle.StandardPixmap.SP_DialogOpenButton)
+        self.button_directory_selector.setIcon(self.button_directory_selector_icon)
+        self.button_directory_selector.setFlat(True)
+        self.label_save_directory = QtWidgets.QLabel('Save Directory:')
+        self.radio_button_open_save_directory_on_close = QtWidgets.QRadioButton('Open On Close')
+        self.radio_button_open_save_directory_on_close.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        self.layout_save_directory_header.addWidget(self.button_directory_selector)
+        self.layout_save_directory_header.addWidget(self.label_save_directory)
+        self.layout_save_directory_header.addWidget(self.radio_button_open_save_directory_on_close)
+        self.line_edit_save_directory_display = QtWidgets.QLineEdit()
+        self.line_edit_save_directory_display.setReadOnly(True)
+        self.layout_save_directory.addLayout(self.layout_save_directory_header)
+        self.layout_save_directory.addWidget(self.line_edit_save_directory_display)
+
+        # Config Section - Render
+        self.layout_config_section.addLayout(self.layout_download_format)
+        self.layout_config_section.addLayout(self.layout_save_directory)
+
+        # Video Section
+        self.layout_video_section = QtWidgets.QVBoxLayout()
+
+        # Video Section - Buttons
+        self.layout_video_section_buttons = QtWidgets.QHBoxLayout()
+        self.button_add_video = QtWidgets.QPushButton('Add Video')
+        self.button_remove_videos = QtWidgets.QPushButton('Remove Video(s)')
+        self.button_download_videos = QtWidgets.QPushButton("Download")
+        self.layout_video_section_buttons.addWidget(self.button_add_video)
+        self.layout_video_section_buttons.addWidget(self.button_remove_videos)
+        self.layout_video_section_buttons.addWidget(self.button_download_videos)
+
+        # Video Section - Video Table Details
+        self.table_view_video_details = QtWidgets.QTableView()
+        self.table_view_video_details.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self.table_view_video_details.setSelectionBehavior(QTableView.SelectionBehavior.SelectRows)
+        self.table_view_video_details.horizontalHeader().setSectionsClickable(False)
+        self.table_view_video_details.horizontalHeader().setHighlightSections(False)
+
+        # Video Section - Render
+        self.layout_video_section.addLayout(self.layout_video_section_buttons)
+        self.layout_video_section.addWidget(self.table_view_video_details)
+
+        # Download Status Section
+        self.layout_download_status_section = QtWidgets.QVBoxLayout()
+        self.label_download_status = QtWidgets.QLabel('Downloading...')
+        self.label_download_status.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.progress_bar_download_status_percentage = QtWidgets.QProgressBar()
+        self.label_download_status.hide()
+        self.progress_bar_download_status_percentage.hide()
+        self.button_clear_downloads = QtWidgets.QPushButton('Clear Table')
+        self.button_clear_downloads.hide()
+
+        # Download Status - Render
+        self.layout_download_status_section.addWidget(self.label_download_status)
+        self.layout_download_status_section.addWidget(self.progress_bar_download_status_percentage)
+        self.layout_download_status_section.addWidget(self.button_clear_downloads)
+
+        # Central Widget - Render
+        self.main_layout.addLayout(self.layout_config_section)
+        self.main_layout.addLayout(self.layout_video_section)
+        self.main_layout.addLayout(self.layout_download_status_section)
+
+        # Signals and Slots Connections
+        self.connect_signals_slots()
+
+        # Tool Tips
+        self.set_tool_tips()
+
+        # App Initialization
+        MainWindow.setCentralWidget(self.central_widget)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    # Signals and Slots
+    def connect_signals_slots(self):
+        pass
+
+    # Tool Tips
+    def set_tool_tips(self):
+        self.combo_box_download_formats.setToolTip('Default download format for added videos')
+        self.button_directory_selector.setToolTip('Select a directory to store downloaded files')
+        self.radio_button_open_save_directory_on_close.setToolTip(
+            'Open the save directory of downloaded videos on app closure')
+        self.button_add_video.setToolTip('Add video to download, provided a url, save name, and format')
+        self.button_remove_videos.setToolTip('Remove currently selected videos')
---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from app.main_window import UIMainWindow
+from PyQt6.QtWidgets import QMainWindow, QApplication, QStyleFactory
+
+
+class MainWindow(QMainWindow, UIMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setup_ui(self)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Fusion'))
+    window = MainWindow()
+    app.exec()
